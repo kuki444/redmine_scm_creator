3f14eff77d4c4b93ef03f8174871166bd8f9b4bb
kuki <kuki@dcr.co.jp>
2021-02-06 15:41:54 +0900

    adapter変更

--- lib/adapters/gitlab_adapter.rb
+++ lib/adapters/gitlab_adapter.rb
@@ -4,7 +4,6 @@ module Redmine
     module Scm
         module Adapters
             class GitlabAdapter < GitAdapter
-
                 def clone
                     cmd_args = %w{clone --mirror}
                     cmd_args << url_with_credentials
@@ -15,7 +14,6 @@ module Redmine
                     Rails.logger.error "gitlab repository cloning failed: #{error.message}"
                     false
                 end
-
                 def fetch
                     Dir.chdir(root_url) do
                         cmd_args = %w{fetch --quiet --all --prune}
@@ -24,9 +22,10 @@ module Redmine
                 rescue ScmCommandAborted => error
                     Rails.logger.error "commits fetching failed: #{error.message}"
                 end
-
+                def api
+                    @api ||= ScmConfig && ScmConfig['gitlab']['api']
+                end
             private
-
                 def url_with_credentials
                     if @login.present? && @password.present?
                         if url =~ %r{\Ahttp://}
@@ -37,10 +36,15 @@ module Redmine
                             url.sub(%r{\Agit@}, "#{@login}:#{@password}@")
                         end
                     else
-                        url
+                        if url =~ %r{\Ahttp://}
+                            url.sub(%r{\Ahttp://}, "http://oauth2:#{api['token']}@")
+                        elsif url =~ %r{\Ahttps://}
+                            url.sub(%r{\Ahttps://}, "https://oauth2:#{api['token']}@")
+                        else
+                            url
+                        end
                     end
                 end
-
             end
         end
     end
